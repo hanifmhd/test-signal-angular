3a2812c7b387d57dccc2b11323e108e4d7111f10
Muhammad Hanif <muhammad_hanif@telkomsel.co.id>
2023-06-06 12:14:45 +0700

    add example implementation of signal-ui on react project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import 'signal-ui';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
